370dacfd6913217a00bc0b7db99d1cc1f90ad953
Sarah Meyer <sarah@macbook-pro-von-sarah.home>
2023-08-10 23:16:14 +0200

    working version

--- travel_agency.rb
+++ travel_agency.rb
@@ -1,4 +1,5 @@
 require_relative './travel_agency_helper'
+require "json"
 
 class TravelAgency
   include TravelAgencyHelper
@@ -6,15 +7,26 @@ class TravelAgency
   def show_country_data
     user_input = read_user_input
     country_data_as_json = fetch_country_data(user_input)
+    country_data = JSON.parse(country_data_as_json)
 
-    # You can remove following two lines, once you don't need them anymore.
-    # puts "\nThis is what country_data_as_json looks like:\n\n"
-    puts country_data_as_json
+     puts "Name: #{country_data[0]["name"]["common"]}"
+    puts "Official Name: #{country_data[0]["name"]["official"]}"
+    spellings = country_data[0]["altSpellings"]
+    joined_spellings = spellings.join(", ")
+    puts "Alternative Spellings: #{joined_spellings}"
+    puts "Capital: #{country_data[0]["capital"][0]}"
+    puts "FIFA Code: #{country_data[0]["fifa"]}"
+    currency_code = country_data[0]["currencies"].keys.first
+    currency_name = country_data[0]["currencies"][currency_code]["name"]
+    currency_symbol = country_data[0]["currencies"][currency_code]["symbol"]
+    puts "Currency: #{currency_name}"
+    borders = country_data[0]["borders"]
+    joined_borders = borders.join(", ")
+    puts "Region: #{country_data[0]["region"]}"
+    puts "Switzerland shares borders with: #{joined_borders}"
 
-    # Todo:
-    # Investigate the output from `country_data_as_json`
-    # Parse the content and display the data you want (see the Examples in the README description).
   end
+  
 end
 
 TravelAgency.new.start
